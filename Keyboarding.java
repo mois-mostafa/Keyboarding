d374e147ba54a7fa1d48344711986fb8202e562a
Keyboarding <mois.mostafa@gmail.com>
2013-05-07 07:23:48 +0300

    Create Keyboarding.java

---
+++ Keyboarding.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keyboarding;
+
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Mostafa
+ */
+public class Keyboarding {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        KeyboardingManifest keyDemoFrame = new KeyboardingManifest();
+        keyDemoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        keyDemoFrame.setSize(350,100);
+        keyDemoFrame.setVisible(true);
+        
+    }
+}
